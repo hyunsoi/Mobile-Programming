e803f7b2cd8bff76641031c0c4bef8ec321dffd3
hyunsoi <102220333+hyunsoi@users.noreply.github.com>
2022-11-09 22:32:39 +0900

    Create FallWinter_SubActivity2.java

---
+++ FallWinter_SubActivity2.java
@@ -0,0 +1,15 @@
+package com.example.mpproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FallWinter_SubActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fall_winter_sub2);
+        setTitle("Fall/Winter Fashion Trend");
+    }
+}
