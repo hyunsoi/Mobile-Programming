20aecd6d24e5d97586b14f56619c2efda2e65c87
hyunsoi <102220333+hyunsoi@users.noreply.github.com>
2022-11-09 22:41:26 +0900

    Create SpringSummer_SubActivity.java

---
+++ SpringSummer_SubActivity.java
@@ -0,0 +1,15 @@
+package com.example.mpproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpringSummer_SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spring_summer_main);
+        setTitle("Spring/Summer Fashion Trend");
+    }
+}
